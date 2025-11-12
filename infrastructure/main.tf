6c480a78a69a2b9442002c339d08d29842b767e5
Gimshi <your_email@example.com>
2025-11-12 15:41:11 +0530

    Add Terraform deployment workflow

---
+++ infrastructure/main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "local" {}
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+variable "aws_region" {
+  default = "ap-south-1"
+}
+
+resource "aws_instance" "medilocate_server" {
+  ami           = "ami-0dee22c13ea7a9a67"
+  instance_type = "t2.micro"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y docker
+              systemctl start docker
+              docker run -d -p 3000:80 gimshi/medilocate_frontend:latest
+              docker run -d -p 4000:4000 gimshi/medilocate_backend:latest
+              EOF
+
+  tags = {
+    Name = "Medilocate-Auto-Deploy"
+  }
+}
